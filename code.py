f5b9d6f2b83f32891e1e72abf3361a2b8ee9219c
@vassu <vasanthapinninti13@gmail.com>
2025-08-14 18:11:42 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,31 @@
+#print("hello")
+#a=-10
+#b=-10
+#print(id(a))
+#print(id(b))
+# name="Harish is my python triner working in 10000 coders in hyd"
+#print(id(name))
+#name1="Harish is my python triner working in 10000 coders in hyd"
+#print(id(name1))
+# list1=["q","w","e","r","t"]
+# x=list1[3]
+# print(x)
+# print(len(list1))
+# list1[3]=108
+# print(list1)
+# x=len(list1) - 2
+# print(list1[x]
+# hii_frnds={"name":"Harish","age":20,"village":"Hyd","Study":"B.tech","hobby":"singing"}
+# x=len(hii_frnds)
+# print(x)
+# hii_frnds["hobby"]="Dcing"
+# print(hii_frnds)
+# hii_frnds["name"]="vassu"
+# print(hii_frnds)
+# del(hii_frnds["age"])
+# print(hii_frnds)
+# hii_frnds["like"]="lotus"
+# print(hii_frnds)
+x=2
+y=5
+print(x+y)
