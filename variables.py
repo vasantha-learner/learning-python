2e2373580c59fb690f6a643cf3f159cff52481bb
@vassu <vasanthapinninti13@gmail.com>
2025-08-14 23:39:05 +0530

    variables

---
+++ variables.py
@@ -0,0 +1,73 @@
+# Variables in python:
+# print("hello")        to display id of -10 value
+#a=-10
+#b=-10
+#print(id(a))
+#print(id(b))
+
+# x=10
+# y=5
+# print(x+y)            #addition
+# print(y-x)
+# print(x%y)
+
+# Multiple assigning
+# a,b,c=1,2,3
+# print(b)
+
+# id
+# name="Harish sir is my python trainer working in 10000 coders in hyd"
+#print(id(name))
+#name1="Harish  sir is my python trainer working in 10000 coders in hyd"
+#print(id(name1))
+# chocolates_i_having=10
+# chocolates_i_ate=3
+# x=chocolates_i_having-chocolates_i_ate
+# print(x)
+
+# x=10
+# y=5
+# print("addition=",x+y)
+# print('substraction=',x-y)
+# print(x*y)
+# print(x/y)
+# print(x//y)
+# print(x**y)
+# print(x%y)
+# print(x^y)
+
+# x=5
+# x+=3
+# print(x)
+
+# x*=3
+# print(x)
+
+# x**=3
+# print(x)
+
+# x-=3
+# print(x)
+
+# x=3
+# y=3
+# print(x==y)
+
+# x=7
+# y=8
+# print(x!=y)
+# print(x>y)
+# print(x<y)
+# print(x>=y)
+# print(x<=y)
+# print(x//y)
+
+# x+=3
+# y-=5
+# print("x,y values:",x,y)
+# x*=3
+# y/=5
+# print("x,y values:",x,y)
+# print(x^2)
+# print(x**2)
+
